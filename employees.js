51ddfcf25a379a62a39868ccfe8636d82566dcf7
erika cueva <ecueva88@gmail.com>
2020-12-30 16:02:40 -0800

    created seed file and wrote js functions to add employees

--- employees.js
+++ employees.js
@@ -25,34 +25,68 @@ function runSearch() {
       type: "rawlist",
       message: "What would you like to do?",
       choices: [
-        "View all departments",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "Find artists with a top song and top album in the same year",
+        "View All Departments",
+        "View All Employees",
+        "View All Roles",
+        "Add Department",
+        "Add Role",
+        // "View All Employees By Department",
+        // "View All Employees By Manager",
+        "Add Employee",
+        // "Remove Employee",
+        "Update Employee Role",
+        // "Update Employee Manager",
       ],
     })
     .then(function (answer) {
       switch (answer.action) {
-        case "View all departments":
+        case "View All Departments":
           viewDepartments();
           break;
 
-        case "Find all artists who appear more than once":
-          multiSearch();
+        case "View All Employees":
+          viewEmployees();
           break;
 
-        case "Find data within a specific range":
-          rangeSearch();
+        case "View All Roles":
+          viewRoles();
           break;
 
-        case "Search for a specific song":
-          songSearch();
+        case "Add Department":
+          addDept();
           break;
 
-        case "Find artists with a top song and top album in the same year":
+        case "Add Role":
+          addRole();
+          break;
+
+        //bonus
+        // case "View All Employees By Department":
+        //   viewEmpDept();
+        //   break;
+
+        //bonus
+        // case "View All Employees By Manager":
+        //   songSearch();
+        //   break;
+
+        case "Add Employee":
+          addEmpl();
+          break;
+
+        //bonus
+        // case "Remove Employee":
+        //   songAndAlbumSearch();
+        //   break;
+
+        case "Update Employee Role":
           songAndAlbumSearch();
           break;
+
+        //bonus
+        // case "Update Employee Manager":
+        //   songAndAlbumSearch();
+        //   break;
       }
     });
 }
@@ -65,132 +99,63 @@ function viewDepartments() {
   });
 }
 
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+function viewEmployees() {
+  var query = "SELECT * FROM employee";
   connection.query(query, function (err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
+    console.table(res);
     runSearch();
   });
 }
 
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-    ])
-    .then(function (answer) {
-      var query =
-        "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
+function viewRoles() {
+  var query = "SELECT * FROM role";
+  connection.query(query, function (err, res) {
+    console.table(res);
+    runSearch();
+  });
 }
 
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
+function addEmpl() {
+  inquirer.prompt([
+    {
+      name: "first_name",
       type: "input",
-      message: "What song would you like to look for?",
-    })
-    .then(function (answer) {
-      console.log(answer.song);
-      connection.query(
-        "SELECT * FROM top5000 WHERE ?",
-        { song: answer.song },
-        function (err, res) {
-          console.log(
-            "Position: " +
-              res[0].position +
-              " || Song: " +
-              res[0].song +
-              " || Artist: " +
-              res[0].artist +
-              " || Year: " +
-              res[0].year
-          );
-          runSearch();
-        }
-      );
-    });
-}
-
-function songAndAlbumSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
+      message: "What's the employee's first name? ",
+    },
+    {
+      name: "last_name",
       type: "input",
-      message: "What artist would you like to search for?",
-    })
-    .then(function (answer) {
-      var query =
-        "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-      query +=
-        "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-      query +=
-        "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-      connection.query(
-        query,
-        [answer.artist, answer.artist],
-        function (err, res) {
-          console.log(res.length + " matches found!");
-          for (var i = 0; i < res.length; i++) {
-            console.log(
-              i +
-                1 +
-                ".) " +
-                "Year: " +
-                res[i].year +
-                " Album Position: " +
-                res[i].position +
-                " || Artist: " +
-                res[i].artist +
-                " || Song: " +
-                res[i].song +
-                " || Album: " +
-                res[i].album
-            );
-          }
-
-          runSearch();
-        }
-      );
+      message: "What's the employee's last name? ",
+    },
+    {
+      name: "role_id",
+      type: "input",
+      message: "What's the employee's role ID? ",
+    },
+    {
+      name: "manager_id",
+      type: "input",
+      message: "What's the employee's manager ID? ",
+    },
+    
+  ])
+  .then(function(answer) {
+    console.log(answer);
+    var query =
+    "INSERT into employee SET ?";
+    connection.query(query, answer, function (err, res) {
+      console.log("Successfully inserted an employee!");
+      runSearch();
     });
+
+  })
+
 }
+
+// function viewEmpDept() {
+//   var query = "SELECT employee FROM department";
+//   connection.query(query, function (err, res) {
+//     console.table(res);
+//     runSearch();
+//   });
+// }
---
+++ seeds.sql
@@ -0,0 +1,25 @@
+USE employees_db;
+
+INSERT into department (name)
+ VALUES 
+ ("marketing"),
+ ("admin"),
+ ("legal"),
+ ("engineering");
+
+
+INSERT into role (title, salary, department_id)
+VALUES ("head engineer", 100000.00, 4),
+("junior engineer", 75000.00, 4),
+("attorney", 100500.00, 3),
+("secretary", 60500.00, 2),
+("designer", 70500.00, 1);
+
+INSERT into employee (first_name, last_name, role_id, manager_id)
+VALUES 
+("Bob", "Smith", 1, NULL),
+("Linda", "Jones", 3, NULL),
+("Joe", "Joey", 2, 1),
+("Nate", "Gonzalez", 4, NULL);
+
+
